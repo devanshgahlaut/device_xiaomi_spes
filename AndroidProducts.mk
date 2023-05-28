145b10ab181fa9ac100cc686b0e3a9e7bcdc603d
devanshgahlaut <gahlautdevansh14@gmail.com>
2023-05-28 20:00:03 +0530

    Revert "spes: Bringup for VoidUI"
    This reverts commit 46a80102080696c53787471509d49d9de9fd464e.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 The VoidUI Project
+# Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_spes.mk
+    $(LOCAL_DIR)/blaze_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_spes-user \
-    aosp_spes-userdebug \
-    aosp_spes-eng
+    blaze_spes-user \
+    blaze_spes-userdebug \
+    blaze_spes-eng
--- blaze_spes.mk
+++ blaze_spes.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The VoidUI Project
+# Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,26 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common VoidUI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := aosp_spes
+PRODUCT_NAME := blaze_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Rom Specific Flags
-TARGET_INCLUDE_WIFI_EXT := true
+# Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_QUICK_TAP  := true
 
-# VoidUI Maintainer Flags
-VOID_MAINTAINER := D-G
-CUSTOM_BUILD_TYPE := UNOFFICIAL
+# Blaze Official Stuff
+BLAZE_BUILD_TYPE := OFFICIAL
+BLAZE_MAINTAINER := sayann70
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
