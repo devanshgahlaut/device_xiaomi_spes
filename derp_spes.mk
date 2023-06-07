c666a1ad459592222e8703fa863c874afe41e078
devanshgahlaut <gahlautdevansh14@gmail.com>
2023-06-07 19:51:53 +0530

    DerpFest Bringup

--- derp_spes.mk
+++ derp_spes.mk
@@ -11,26 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common VoidUI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := aosp_spes
+PRODUCT_NAME := derp_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
 PRODUCT_MANUFACTURER := Xiaomi
-
-# Rom Specific Flags
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_QUICK_TAP  := true
-
-# VoidUI Maintainer Flags
-VOID_MAINTAINER := D-G
-CUSTOM_BUILD_TYPE := UNOFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
